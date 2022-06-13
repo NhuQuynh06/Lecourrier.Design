6d28b7e5622aa7c2e39bcef765ee93df6fdd59e4
CPU60417\quynhttn3 <quynhtran0618@gmail.com>
2022-06-13 10:20:22 +0700

    style pc

--- dist/desktop/js/ui.js
+++ dist/desktop/js/ui.js
@@ -5,17 +5,28 @@ $(document).ready(function () {
         slidesToScroll: 1,
         arrows: true,
         autoplay: true,
+        speed: 1000,
         dots: true,
     });
 
     $('.slide-wrapper .box-content').slick({
         centerMode: true,
         centerPadding: '473px',
+        // centerPadding: '320px',
         slidesToShow: 1,
         focusOnSelect: true,
         dots: true,
         infinite: true,
         speed: 1000,
         autoplay: true,
+        // responsive: [
+        //     {
+        //         breakpoint: 1366,
+        //         settings: {
+        //             centerMode: true,
+        //             centerPadding: '400px',
+        //         }
+        //     }
+        // ]
     });
 });
